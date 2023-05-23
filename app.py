946f17378d87952676444162e3d2b3c199795380
roy <roeilat@gmail.com>
2023-05-23 14:28:49 +0300

    first upload to git of all the files

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, jsonify
+import requests
+
+#test
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("HomePage.html")
+
+@app.route("/eth")
+def eth():
+    # Make a GET request to the CoinDesk API
+    eth_response = requests.get("https://api.coinstats.app/public/v1/coins/ethereum")
+
+    # Extract the Ethereum price from the API response
+    if eth_response.status_code == 200:
+        eth_price = eth_response.json()["coin"]["price"]
+    
+    # Pass the Bitcoin price data to the template
+    return render_template("eth.html", eth_price=eth_price)
+
+@app.route("/btc")
+def btc():
+    # Make a GET request to the CoinDesk API
+    btc_response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    
+    # Extract the Bitcoin price from the API response
+    if btc_response.status_code == 200:
+        bitcoin_price = btc_response.json()["bpi"]["USD"]["rate"]
+ 
+    # Pass the Bitcoin price data to the template
+    return render_template("btc.html", bitcoin_price=bitcoin_price)
+
+
+if __name__ == '__main__':
+    app.run()
