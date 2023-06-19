bc538e958eedf120dfac2c656c074e01bc060918
Roy-Latin <roeilat@gmail.com>
2023-06-19 12:10:55 +0300

    fix

--- tests.sh
+++ tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -xe
-curl -s ${env.EC2_IP_TEST}:5000
-curl -s ${env.EC2_IP_TEST}:5000 | grep Crypto
-curl -s ${env.EC2_IP_TEST}:5000/eth
-curl -s ${env.EC2_IP_TEST}:5000/btc
-curl -s ${env.EC2_IP_TEST}:5000/ada
-curl -s ${env.EC2_IP_TEST}:5000/xrp
+curl -s ${EC2_IP_TEST}:5000
+curl -s ${EC2_IP_TEST}:5000 | grep Crypto
+curl -s ${EC2_IP_TEST}:5000/eth
+curl -s ${EC2_IP_TEST}:5000/btc
+curl -s ${EC2_IP_TEST}:5000/ada
+curl -s ${EC2_IP_TEST}:5000/xrp
