183c2d1e75e2354760a148fb0be26f34e690f4f4
Roy-Latin <roeilat@gmail.com>
2023-06-19 11:45:13 +0300

    fix

--- tests.sh
+++ tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -xe
-curl -s ${EC2_IP_TEST}:5000
-curl -s ${EC2_IP_TEST}:5000 | grep Crypto
-curl -s ${EC2_IP_TEST}:5000/eth
-curl -s ${EC2_IP_TEST}:5000/btc
-curl -s ${EC2_IP_TEST}:5000/ada
-curl -s ${EC2_IP_TEST}:5000/xrp
+curl -s 18.212.98.180:5000
+curl -s 18.212.98.180:5000 | grep Crypto
+curl -s 18.212.98.180:5000/eth
+curl -s 18.212.98.180:5000/btc
+curl -s 18.212.98.180:5000/ada
+curl -s 18.212.98.180:5000/xrp
