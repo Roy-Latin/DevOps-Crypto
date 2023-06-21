9f09d7509e4461ff400c271c11006647067c3350
Roy-Latin <roeilat@gmail.com>
2023-06-21 11:28:23 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 sudo yum install python -y
 sudo yum install python-pip -y
 sudo pip install ansible
-ansible-playbook requirements.yml
-ansible-playbook deploy.yml
+ansible-playbook DevOps-Crypto/requirements.yml
+ansible-playbook DevOps-Crypto/deploy.yml
