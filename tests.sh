c814fdcfc36f6d776a9b2fda8044516abbc1da1a
Roy-Latin <roeilat@gmail.com>
2023-07-10 15:16:22 +0300

    db

--- tests.sh
+++ tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -xe
 curl -s $EC2_IP_TEST:5000
 curl -s $EC2_IP_TEST:5000 | grep Crypto
-curl -s $EC2_IP_TEST:5000/eth | grep price
-curl -s $EC2_IP_TEST:5000/btc | grep price
-curl -s $EC2_IP_TEST:5000/ada | grep price
-curl -s $EC2_IP_TEST:5000/xrp | grep price
+curl -s $EC2_IP_TEST:5000/eth | grep $
+curl -s $EC2_IP_TEST:5000/btc | grep $
+curl -s $EC2_IP_TEST:5000/ada | grep $
+curl -s $EC2_IP_TEST:5000/xrp | grep $
