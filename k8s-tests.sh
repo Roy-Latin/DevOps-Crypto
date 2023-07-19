6af1cc92c36eb6c2d9e3b2ee695331fab8335558
roy <roy@ubuntu.myguest.virtualbox.org>
2023-07-19 11:21:04 +0300

    test

--- k8s-tests.sh
+++ k8s-tests.sh
@@ -5,8 +5,29 @@ timeout=10
 
 # Make curl requests with a timeout
 curl -s --max-time $timeout 192.168.56.1
-curl -s --max-time $timeout 192.168.56.1 | grep Crypto
-curl -s --max-time $timeout 192.168.56.1/eth | grep lalalal
-curl -s --max-time $timeout 192.168.56.1/btc | grep Price
-curl -s --max-time $timeout 192.168.56.1/ada | grep Price
-curl -s --max-time $timeout 192.168.56.1/xrp | grep Price
+
+# Check patterns using grep and handle the cases
+if ! curl -s --max-time $timeout 192.168.56.1 | grep -q Crypto; then
+  echo "Error: Pattern 'Crypto' not found"
+  exit 1
+fi
+
+if ! curl -s --max-time $timeout 192.168.56.1/eth | grep -q lalalal; then
+  echo "Error: Pattern 'lalalal' not found"
+  exit 1
+fi
+
+if ! curl -s --max-time $timeout 192.168.56.1/btc | grep -q Price; then
+  echo "Error: Pattern 'Price' not found"
+  exit 1
+fi
+
+if ! curl -s --max-time $timeout 192.168.56.1/ada | grep -q Price; then
+  echo "Error: Pattern 'Price' not found"
+  exit 1
+fi
+
+if ! curl -s --max-time $timeout 192.168.56.1/xrp | grep -q Price; then
+  echo "Error: Pattern 'Price' not found"
+  exit 1
+fi
