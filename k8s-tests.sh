540077530e303900246842392249a5c066077892
roy <roy@ubuntu.myguest.virtualbox.org>
2023-07-19 11:06:59 +0300

    test

--- k8s-tests.sh
+++ k8s-tests.sh
@@ -6,7 +6,7 @@ timeout=10
 # Make curl requests with a timeout
 curl -s --max-time $timeout 192.168.56.1
 curl -s --max-time $timeout 192.168.56.1 | grep Crypto
-curl -s --max-time $timeout 192.168.56.1/eth | grep Price
+curl -s --max-time $timeout 192.168.56.1/eth | grep lalalal
 curl -s --max-time $timeout 192.168.56.1/btc | grep Price
 curl -s --max-time $timeout 192.168.56.1/ada | grep Price
 curl -s --max-time $timeout 192.168.56.1/xrp | grep Price
