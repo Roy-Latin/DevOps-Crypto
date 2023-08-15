0ec14828722656de36e31844c8f72678b61581e1
Roy-Latin <roeilat@gmail.com>
2023-08-15 15:24:55 +0300

    fix

--- scripts/helm-versions.sh
+++ scripts/helm-versions.sh
@@ -4,4 +4,4 @@
 NEW_VERSION=0.1.${BUILD_NUMBER}  # Replace this with the actual new version
 
 # Update the version in charts.yaml
-sed -i "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $NEW_VERSION/g" charts.yaml
+sed -i "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $NEW_VERSION/g" my-flask-app/Chart.yaml
