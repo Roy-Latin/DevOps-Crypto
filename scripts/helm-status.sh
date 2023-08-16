1065932d02a8c6db49e8e22fbf5061085c6d89b8
Roy-Latin <roeilat@gmail.com>
2023-08-16 10:18:59 +0300

    fix helm upgrade

--- scripts/helm-status.sh
+++ scripts/helm-status.sh
@@ -7,5 +7,5 @@ expected_error="Error: release: not found"
 if [ "$command_output" = "$expected_error" ]; then
     helm install crypto-helm my-flask-app/
 else
-    helm upgrade crypto-helm my-flask-app/
+    helm upgrade --recreate-pods crypto-helm my-flask-app/
 fi
