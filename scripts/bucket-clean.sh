e7d8ce8282d74d72c0d960dc65d21f1ae0ea6fa6
Roy-Latin <roeilat@gmail.com>
2023-08-24 14:45:17 +0300

    bucket clean fix

--- scripts/bucket-clean.sh
+++ scripts/bucket-clean.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-BUCKET_NAME="helm-crypto-k8s"
-DIRECTORY="charts"
+BUCKET_NAME="crypto-site-helm"
 
 # List all .tgz files in the specified directory and delete them one by one
-gsutil ls "gs://$BUCKET_NAME/$DIRECTORY/*.tgz" | while read -r tgz_object; do
+gsutil ls "gs://$BUCKET_NAME/*.tgz" | while read -r tgz_object; do
     gsutil rm "$tgz_object"
 done
