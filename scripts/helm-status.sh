558e2d52f38af5ea9f5f65f49f2065010ead61f8
Roy-Latin <roeilat@gmail.com>
2023-08-24 14:40:20 +0300

    fixed gcloud connection

--- scripts/helm-status.sh
+++ scripts/helm-status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-command_output=$(helm status crypto-helm 2>&1)
+command_output=$(helm status crypto-app 2>&1)
 
 expected_error="Error: release: not found"
 
 if [ "$command_output" = "$expected_error" ]; then
-    helm install crypto-helm my-flask-app/
+    helm install crypto-app crypto-site-helm/my-flask-app
 else
-    helm upgrade --recreate-pods crypto-helm my-flask-app/
+    helm upgrade --recreate-pods crypto-app crypto-site-helm/my-flask-app
 fi
