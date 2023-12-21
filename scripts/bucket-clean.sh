c509ab507b233aa12b6540740c76207c5fbb83b8
Roy-Latin <134203398+Roy-Latin@users.noreply.github.com>
2023-12-21 14:53:54 +0000

    fixing bucket name

--- scripts/bucket-clean.sh
+++ scripts/bucket-clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUCKET_NAME="crypto-site-helm"
+BUCKET_NAME="roys-crypto-site"
 
 # List all .tgz files in the specified directory and delete them one by one
 gsutil ls "gs://$BUCKET_NAME/*.tgz" | while read -r tgz_object; do
